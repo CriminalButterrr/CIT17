88603268a24807d2309d5c4e62c1045e28a6f794
CriminalButterrr <h.agwiking@gmail.com>
2023-10-18 11:05:19 +0800

    pushing into new branch

--- Calculator/index.php
+++ Calculator/index.php
@@ -7,29 +7,32 @@
     <title>Calculator</title>
 </head>
 <body>
-    <h1>SIMPLE CALCULATOR</h1>
-    <form name="calc" action="actions.php" method="post">
-        <table>
-            <tr>
-                <td>
-                    <label for="input1">First number</label><br>
-                    <input type="number" name="input1" id="input1">
-                </td>
-                <td>
-                    <label for="input2">Second number</label><br>
-                    <input type="number" name="input2" id="input2">
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                <input type="submit" value="+" name="operator">
-                <input type="submit" value="-" name="operator">
-                <input type="submit" value="*" name="operator">
-                <input type="submit" value="/" name="operator">
-                </td>
-            </tr>
-        </table>    
-    </form>
+    <div class="container">
+        <h1>SIMPLE CALCULATOR</h1>
+        <form name="calc" action="actions.php" method="post">
+            <table>
+                <tr>
+                    <td>
+                        <label for="input1">First number</label><br>
+                        <input type="number" name="input1" id="input1">
+                    </td>
+                    <td>
+                        <label for="input2">Second number</label><br>
+                        <input type="number" name="input2" id="input2">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                    <input type="submit" value="+" name="operator">
+                    <input type="submit" value="-" name="operator">
+                    <input type="submit" value="*" name="operator">
+                    <input type="submit" value="/" name="operator">
+                    </td>
+                </tr>
+            </table>    
+        </form>
+    </div>
+   
     
 </body>
 </html>
