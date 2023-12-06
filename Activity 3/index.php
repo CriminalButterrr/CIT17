330049d18a03ed3fc1efdbf13e5b25be29783348
Harold Agwiking <h.agwiking@gmail.com>
2023-12-06 09:16:33 +0800

    index update

--- Activity 3/index.php
+++ Activity 3/index.php
@@ -9,10 +9,35 @@
 </head>
 <body>
     <div class="container-fluid">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addUserModal">Add User</button>
-        <a class="btn btn-secondary" href="select.php">View Users Table</a>
-        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#updateUserModal">Update User</button>
-        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteUserModal">Delete User</button>
+        <div class="d-flex flex-row justify-content-center my-3">
+            <button type="button" class="btn btn-primary mx-2" data-bs-toggle="modal" data-bs-target="#addUserModal">Add User</button>
+            <button type="button" class="btn btn-warning mx-2" data-bs-toggle="modal" data-bs-target="#updateUserModal">Update User</button>
+            <button type="button" class="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#deleteUserModal">Delete User</button>
+        </div>
+        <div class="row">            
+            <?php
+                include_once("dbConnect.php");
+                $sql = "SELECT id, username, email FROM users";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    echo '<table class="table"> <thead> <tr> <th class="col">ID</th> <th class="col">Username</th> <th class="col">Email</th> </tr> </thead>';
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tbody> <tr>";
+                        echo "<td>" . $row["id"] . "</td>";
+                        echo "<td>" . $row["username"] . "</td>";
+                        echo "<td>" . $row["email"] . "</td>";
+                        echo "</tr> </tbody>";
+                    }
+                    echo '</table>';
+                } else {
+                    echo "0 results";
+                }
+            ?>
+        </div>
+        
+
+    
     </div>
     <!-- Add User Modal-->
     <div class="modal fade" id="addUserModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
@@ -45,6 +70,7 @@
         </div>
     </div>
     </div>
+    <!-- Update User Modal-->
     <div class="modal fade" id="updateUserModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
@@ -75,6 +101,7 @@
         </div>
     </div>
     </div>
+    <!-- Delete User Modal-->
     <div class="modal fade" id="deleteUserModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
@@ -100,5 +127,6 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 </html>
